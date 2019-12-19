661c4f354bdddfcf5cadf8c469adaac6788c795c
dylan orgeron <dylan.orgeron@gmail.com>
2019-12-19 16:27:43 -0600

    proof of concept

--- scripts/index.ts
+++ scripts/index.ts
@@ -20,15 +20,42 @@ let colors = [
 function generateTable() {
     const table = document.getElementById('color-table')
     let tableCoreHtml = ``
-    let tableHeaderHtml = `<div class="color-row">`
-    colors.forEach(bgColor => {
-        tableHeaderHtml += `<div class="color-cell" style="background: ${bgColor};color: ${bgColor}">${bgColor}</div>`
-        tableCoreHtml += `<div class="color-row" style='background: ${bgColor}'>`
-        colors.forEach(fgColor => tableCoreHtml += `<div class="color-cell" style="color: ${fgColor}">${fgColor}</div>`)
+    let tableHeaderHtml = `<div class="color-row color-header">`
+    colors.forEach((bgColor, index) => {
+        tableHeaderHtml +=
+            `<div 
+            id="header-${index}"
+            class="color-cell" 
+            style="background: ${bgColor};color: ${bgColor}">
+            ${bgColor}
+            </div>
+            <input style="display:none" type="color" id="input-${index}" value="${bgColor}"/>`
+        tableCoreHtml +=
+            `<div class="color-row" style='background: ${bgColor}'>`
+        colors.forEach(fgColor =>
+            tableCoreHtml +=
+            `<div class="color-cell" style="color: ${fgColor}">${fgColor}</div>`
+        )
         tableCoreHtml += `</div>`
     })
     tableHeaderHtml += `</div>`
     if (table) table.innerHTML = tableHeaderHtml + tableCoreHtml
+    for (let i = 0; i < 16; i++) {
+        const headerEl = document.getElementById(`header-${i.toString()}`)
+        if (headerEl) {
+            headerEl.addEventListener('click', () => {
+                const el = document.getElementById(`input-${i}`)
+                if(el) el.click()
+            })
+        }
+        const inputEl = document.getElementById(`input-${i.toString()}`)
+        if(inputEl){
+            inputEl.addEventListener('change', (e: any) =>{
+                colors[i] = inputEl.value
+                generateTable()
+            })
+        }
+    }
 }
 
 generateTable()
