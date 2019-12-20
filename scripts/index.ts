ffbebd3ce07aa867b230959596e32577a2f46ba7
dylan orgeron <dylan.orgeron@gmail.com>
2019-12-20 15:41:09 -0600

    prune and split code

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,5 +1,7 @@
-import ColorThief from 'colorthief'
+// import ColorThief from 'colorthief'
+import ColorTable from './color-table'
 
+//initial palette
 let colors = [
     '#121920',
     '#121E28',
@@ -19,44 +21,6 @@ let colors = [
     '#646963'
 ]
 
-function generateTable() {
-    const table = document.getElementById('color-table')
-    let tableCoreHtml = ``
-    let tableHeaderHtml = `<div class="color-row color-header">`
-    colors.forEach((bgColor, index) => {
-        tableHeaderHtml +=
-            `<div 
-            id="header-${index}"
-            class="color-cell" 
-            style="background: ${bgColor};color: ${bgColor}">
-            </div>
-            <input style="display:none" type="color" id="input-${index}" value="${bgColor}"/>`
-        tableCoreHtml +=
-            `<div class="color-row" style='background: ${bgColor}'>`
-        colors.forEach(fgColor =>
-            tableCoreHtml +=
-            `<div class="color-cell" style="color: ${fgColor}">${fgColor}</div>`
-        )
-        tableCoreHtml += `</div>`
-    })
-    tableHeaderHtml += `</div>`
-    if (table) table.innerHTML = tableHeaderHtml + tableCoreHtml
-    for (let i = 0; i < 16; i++) {
-        const headerEl = document.getElementById(`header-${i.toString()}`)
-        if (headerEl) {
-            headerEl.addEventListener('click', () => {
-                const el = document.getElementById(`input-${i}`)
-                if(el) el.click()
-            })
-        }
-        const inputEl = document.getElementById(`input-${i.toString()}`)
-        if(inputEl){
-            inputEl.addEventListener('change', (e: any) =>{
-                colors[i] = inputEl.value
-                generateTable()
-            })
-        }
-    }
-}
-
-generateTable()
+//build table
+const colorTable = new ColorTable()
+colorTable.generateTable(colors)
---
+++ scripts/vs-code-theme.ts
@@ -0,0 +1,1 @@
+import JSZip from 'jszip'
