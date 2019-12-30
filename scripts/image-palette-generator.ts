25555dacf21b515fefb77db344ceea394208e6ba
dylan orgeron <dylan.orgeron@gmail.com>
2019-12-30 16:50:45 -0600

    wip

--- scripts/image-palette-generator.ts
+++ scripts/image-palette-generator.ts
@@ -1,12 +1,14 @@
 import ColorThief from 'colorthief'
+import ColorContrastChecker from 'color-contrast-checker'
 import { colorTable } from './index'
 
-class ImagePaletteGenerator{
+class ImagePaletteGenerator {
     private colorThief = new ColorThief()
-    constructor(){
+    private ccc = new ColorContrastChecker()
+    constructor() {
         const imageUploader = document.getElementById('image-uploader')
         const imageContainer = document.getElementById('image-container')
-        if(imageUploader && imageContainer){
+        if (imageUploader && imageContainer) {
             imageUploader.addEventListener('change', (e: Event) => {
                 const file = e.target.files[0]
                 const img = document.createElement('img')
@@ -15,9 +17,9 @@ class ImagePaletteGenerator{
                 reader.readAsDataURL(file)
                 imageContainer.innerHTML = ''
                 imageContainer.append(img)
-                if(img.complete){
+                if (img.complete) {
                     this.generateColors(img)
-                }else{
+                } else {
                     img.addEventListener('load', () => {
                         this.generateColors(img)
                     })
@@ -25,7 +27,7 @@ class ImagePaletteGenerator{
             })
         }
     }
-    generateColors(img: HTMLImageElement){
+    generateColors(img: HTMLImageElement) {
         //fetch the starting palette from the image
         let rgbColors = this.colorThief.getPalette(img, 16)
         //try and line the colors up
@@ -37,58 +39,92 @@ class ImagePaletteGenerator{
         colorTable.generateTable()
     }
 
-    convertColorsToHex(colors: []){
+    convertColorsToHex(colors: []) {
         let hexColors: string[] = []
         colors.forEach(color => {
-            hexColors.push(this.rgbToHex(color[0], color[1], color[2]))       
+            hexColors.push(this.rgbToHex(color))
         });
         return hexColors
     }
-    rgbToHex (r: number, g: number, b:number) {
+    rgbToHex(color: []) {
+        const r = color[0]
+        const g = color[1]
+        const b = color[2]
         return '#' + [r, g, b].map(x => {
-        const hex = x.toString(16)
-        return hex.length === 1 ? '0' + hex : hex
-      }).join('')
+            const hex = x.toString(16)
+            return hex.length === 1 ? '0' + hex : hex
+        }).join('')
     }
 
-    sortColors(colors: []){
+    sortColors(colors: []) {
+        
         //lets sort dem colors
         let darkestValue = 1000
         let darkestIndex = 0
-        let secondDarkestValue = 1000
-        let secondDarkestIndex = 0
         let redIndex = 0
         let redRatio = 0
+        let lightestValue = 0
+        let lightestIndex = 0
+        let contrastColors: [] = []
         colors.forEach((color, index) => {
-            //darkest value
+            //darkest value to use for background
+            //shades will be generated later based on this value
             const sum = color[0] + color[1] + color[2]
-            if(sum < darkestValue){
+            if (sum < darkestValue) {
                 darkestValue = sum
                 darkestIndex = index
             }
-            //red
-            if(color[0] / (color[1] + color[2]) > redRatio){
+            //foreground color
+            if(sum > lightestValue){
+                lightestValue = sum
+                lightestIndex = index
+            }
+            //red, for errors
+            if (color[0] / (color[1] + color[2]) > redRatio) {
                 redRatio = color[0] / (color[1] + color[2])
                 redIndex = index
             }
         });
-        //set bg color
-        this.swapColor(0, darkestIndex, colors)
-        //set secondary bg color
-        this.lightenColor(colors[1], colors[0], 5)
+
+        //now that we have our background color, we can check contrast
+        colors.forEach((color, index) => {
+            if(index === lightestIndex || index === redIndex) return
+            if(this.ccc.isLevelCustom(this.rgbToHex(color), this.rgbToHex(colors[darkestIndex]), 3)){
+                contrastColors.push(color)
+            }
+        })
+        console.log(contrastColors)
+
         //red
         this.swapColor(8, redIndex, colors)
-        
+
+        //bg color
+        this.swapColor(0, darkestIndex, colors)
+
+        //foreground color
+        this.swapColor(6, lightestIndex, colors)
+
+        //secondary foreground
+        this.swapColor(6, lightestIndex, colors)
+        this.adjustColor(colors[5], colors[6], -50)
+
+        //secondary shades
+        this.adjustColor(colors[1], colors[0], 5)
+        this.adjustColor(colors[2], colors[1], 15)
+        this.adjustColor(colors[3], colors[2], 25)
+        this.adjustColor(colors[4], colors[3], 25)
+
+        //start using contrast approved colors
         return colors
     }
-    swapColor(index1: any, index2: number, arr: []){
+    swapColor(index1: any, index2: number, arr: []) {
         const colorToSwap = arr[index1]
         arr[index1] = arr[index2]
         arr[index2] = colorToSwap
         return arr
     }
 
-    lightenColor(colorToLighten: [], colorToSample: [], amount: number){
+    adjustColor(colorToLighten: [], colorToSample: [], amount: number) {
         colorToLighten[0] = colorToSample[0] + amount > 255 ? 255 : colorToSample[0] + amount
         colorToLighten[1] = colorToSample[1] + amount > 255 ? 255 : colorToSample[1] + amount
         colorToLighten[2] = colorToSample[2] + amount > 255 ? 255 : colorToSample[2] + amount
